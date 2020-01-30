8f6e469626fe9c5d1c2d44cfb901dba202c76dce
maria liza <mariamartinez@pursuit.org>
2020-01-29 21:17:20 -0500

    resend userpage js html

--- frontend/userPage/userPage.js
+++ frontend/userPage/userPage.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+  // // logo/header
+  // let header = document.querySelector("#logo");
+  // let logoPic = document.querySelector("#logoPic");
+
+  // nav bar/ table of contents
+  let tableOfContents = document.querySelector("#tableOfContents");
+  let tOCList = document.querySelector("#tOCList");
+
+  // left side - user page : pic, name
+  let leftSideDiv = document.querySelector("#leftSideDiv");
+  let profileImg = document.querySelector("#profileImg");
+  let profilePic = document.querySelector("#profilePic");
+  let userName = document.querySelector("#userName");
+  //albums and alum list
+  let albumsSection = document.querySelector("#albumsSection");
+  let userAlbumCollection = document.querySelector("#userAlbumCollection");
+  let userAlbums = document.querySelector("#userAlbums");
+
+  //about me section/ right
+  let aboutMe = document.querySelector("#aboutMe");
+  let userNameAboutMe = document.querySelector("#userNameAboutMe");
+  let userAge = document.querySelector("#userAge");
+  let aboutStatement = document.querySelector("#aboutStatement");
 });
