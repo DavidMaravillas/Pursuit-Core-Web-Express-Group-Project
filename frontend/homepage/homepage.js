690ca3c096d1c0784d219c1c21fb4a1a0f8a7517
maria liza <mariamartinez@pursuit.org>
2020-02-02 15:38:08 -0500

    begin front end setup

---
+++ frontend/homepage/homepage.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // // logo/header
+  // let header = document.querySelector("#logo");
+  // let logoPic = document.querySelector("#logoPic");
+
+  // nav bar/ table of contents
+  let tableOfContentsHomePage = document.querySelector("#tOCListHomePage");
+  let tOCListHP = document.querySelector("#tOCListHP");
+
+  // Center : latest post with user caption and users comments
+  let postsFeed = document.querySelector("#postsFeed");
+});
