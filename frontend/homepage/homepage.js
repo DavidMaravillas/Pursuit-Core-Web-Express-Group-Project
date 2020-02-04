366da0c3edc421cc9cf65546404df8f979fccc71
maria liza <mariamartinez@pursuit.org>
2020-02-03 20:31:21 -0500

    add css elements to homepage, generate newest post on page load

--- frontend/homepage/homepage.js
+++ frontend/homepage/homepage.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // // logo/header
-  // let header = document.querySelector("#logo");
-  // let logoPic = document.querySelector("#logoPic");
-
-  // nav bar/ table of contents
-  let tableOfContentsHomePage = document.querySelector("#tOCListHomePage");
-  let tOCListHP = document.querySelector("#tOCListHP");
-
   // Center : latest post with user caption and users comments
+
   let postsFeed = document.querySelector("#postsFeed");
+
+  const loadNewPosts = async () => {
+    try {
+      //   debugger;
+      let res = await axios.get("http://localhost:3000/posts");
+      res.data.body.forEach(post => {
+        console.log(post);
+        let latestPostP = document.createElement("p");
+        latestPostP.id = "latestPostP"
+        // post.body = res.data.body.body
+        latestPostP.innerHTML = post.body;
+        postsFeed.appendChild(latestPostP);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  loadNewPosts();
 });
+
+loadNewPosts();
