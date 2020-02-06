7eb1a98194ab70e980346659615e56aa3f9efa9c
maria liza <mariamartinez@pursuit.org>
2020-02-05 21:36:22 -0500

    work on number of likes

--- frontend/homepage/homepage.js
+++ frontend/homepage/homepage.js
@@ -27,21 +27,41 @@ document.addEventListener("DOMContentLoaded", () => {
     try {
       let res = await axios.get("http://localhost:3000/posts");
       res.data.body.forEach(post => {
-        let latestPostP = document.createElement("p");
-
-        latestPostP.innerHTML = post.body;
-        // let h3 = document.createElement("h3");
-        // h3.innerText = option.innerText;
-        // postsFeed.appendChild(h3);
+        let latestPostP = document.createElement("div");
         latestPostP.className = "latestPostP";
         postsFeed.appendChild(latestPostP);
+
+        let h3 = document.createElement("h3");
+        h3.innerText = post.body;
+        latestPostP.appendChild(h3);
         let timeP = document.createElement("p");
         timeP.className = "timeP";
         timeP.innerHTML = post.created_at;
-        postsFeed.appendChild(timeP);
-        latestPostP.postID = post.id;
+        latestPostP.appendChild(timeP);
+        // latestPostP.postID = post.id;
 
         let likeBtn = document.createElement("img");
+        let numlike = document.createElement("p");
+
+        // const likes = async userPost => {
+        //   let numOfLike = "";
+        //   try {
+        //     let likes = await axios.get(
+        //       `http://localhost:3000/likes/posts/${userPost.id}`
+        //     );
+        //     debugger;
+        //   } catch (err) {
+        //     console.log(err);
+        //   }
+        //   return numOfLike;
+        // };
+
+        likes = axios.get(`http://localhost:3000/likes/posts/${userPost.id}`);
+
+        numOfLike = likes.data.body.length;
+        debugger;
+        numlike.innerText = numOfLike;
+        latestPostP.appendChild(numlike);
         let unliked =
           "https://www.onlygfx.com/wp-content/uploads/2016/05/hand-drawn-heart-4.png";
         // "https://cdn0.iconfinder.com/data/icons/healthcare-medicine/512/heart-512.png";
@@ -52,15 +72,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
         likeBtn.src = unliked;
 
-        likeBtn.postId = post.id;
+        // likeBtn.postId = post.id;
         likeBtn.className = "likeBtn";
-        postsFeed.appendChild(likeBtn);
+        latestPostP.appendChild(likeBtn);
+        // debugger;
 
         let commentBtn = document.createElement("img");
         commentBtn.src =
           "https://www.freeiconspng.com/uploads/speech-bubble-png-10.png";
         commentBtn.className = "commentBtn";
-        postsFeed.appendChild(commentBtn);
+        latestPostP.appendChild(commentBtn);
 
         likeBtn.addEventListener("dblclick", async e => {
           try {
@@ -76,10 +97,11 @@ document.addEventListener("DOMContentLoaded", () => {
         likeBtn.addEventListener("click", async e => {
           let option = document.querySelector(".optionTag");
           try {
+            // debugger;
             let res = await axios.delete(
               `http://localhost:3000/likes/${e.target.postId}/${option.value}`
             );
-            debugger;
+            // debugger;
             // console.log(option.innerText);
             likeBtn.src = unliked;
           } catch (err) {
